75d764e1f2d36258b2ad02c3d121bd1f57e5a254
Sboutabaa <boutabaa.sami@hotmail.com>
2024-03-10 20:04:38 +0100

    stupid error 01

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A basic gomod2nix flake";
+  description = "default empty template";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
